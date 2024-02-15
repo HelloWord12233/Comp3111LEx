7a01a4ae50d0a9ee847d6cba3c8b7041908a5551
kkkio <hinchanllzz2@gmail.com>
2024-02-15 14:12:04 +0800

    add 3rd comment line

--- src/main/java/Lab1/MainApp1.java
+++ src/main/java/Lab1/MainApp1.java
@@ -4,6 +4,7 @@ public class MainApp1 {
     public static void main(String[] args){
         int b = 2;
         int n = 11;
+        //adding new comment line
         System.out.println("Welcome to Scientific Calculator");
         System.out.println("Program started...");
         System.out.println(b+" to power " + n+" = "+ myLibrary.Power(b, n));
